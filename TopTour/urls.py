55f464b75b0aa3136375840c005d4922984f1b09
Logan Belews <Loganbelew2@gmail.com>
2023-09-12 16:08:16 -0500

    added comment view

--- TopTour/urls.py
+++ TopTour/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from toptourapi.views import register_user, login_user, AttractionView, CategoryView
+from toptourapi.views import register_user, login_user, AttractionView, CategoryView, CommentView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'attractions', AttractionView, 'attraction')
 router.register(r'categories', CategoryView, 'category')
+router.register(r'comments', CommentView, 'comment')
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
