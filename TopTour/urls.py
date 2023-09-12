a37c8b38922803e68b88fb66b12f5c3f90e33c39
Logan Belews <Loganbelew2@gmail.com>
2023-09-12 15:14:46 -0500

    more fixtures and category view

--- TopTour/urls.py
+++ TopTour/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
-from toptourapi.views import register_user, login_user, AttractionView
+from toptourapi.views import register_user, login_user, AttractionView, CategoryView
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'attractions', AttractionView, 'attraction')
+router.register(r'categories', CategoryView, 'category')
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
