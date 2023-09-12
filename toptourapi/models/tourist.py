436f67566877550f6415ff545325c95c3aa43a6b
Logan Belews <Loganbelew2@gmail.com>
2023-09-12 13:13:13 -0500

    changes to tourist model

--- toptourapi/models/tourist.py
+++ toptourapi/models/tourist.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 class Tourist(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=50)
-    profile_image = models.CharField(max_length=100)
+    profile_image = models.CharField(max_length=100, null=True, blank=True)
